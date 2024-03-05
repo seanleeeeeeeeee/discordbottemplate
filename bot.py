1300da8aeafddf9c1547ddf4191cf42094b03070
seanleeeeeeeeee <160643982+seanleeeeeeeeee@users.noreply.github.com>
2024-03-05 17:56:21 +0000

    eeeee

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
 from discord.ext.commands import Bot,Context
 from discord import Intents
 from random import randint
+from googleapiclient.discovery import build
+import pprint
+
+my_api_key = "AIzaSyBYIdPNigKKqZobSX09x_w-JC_HRVyibF8"
+my_cse_id = "16875384dfc844459"
+
+def google(search_term, api_key, cse_id, **kwargs):
+    service = build("customsearch", "v1", developerKey=api_key)
+    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+    return res['items']
+
+
+
 bot = Bot('>',intents=Intents.all())
-
-# from here you can edit to make more commands
-
 
 def add(num: int, num2 : int):
     return f"{num + num2}"
@@ -27,7 +37,7 @@ async def setnumber(ctx: Context):
     num = randint(1, 100)
 
 @bot.command()
-async def guess(ctx: Context. num1: int):
+async def guess(ctx: Context, num1: int):
     global num
     if num1 != num:
         await ctx.send("no ur retarded")
@@ -46,4 +56,17 @@ async def calc(ctx: Context, opt: str, num1: int, num2: int):
     elif opt == "div":
         await ctx.send(div(num1, num2))
 
-bot.run("MTIwOTU1MTEyNjc3MzUwMjAwMw.GnHKkm.NCNiOzd87Xx0u6g19KuMKj5lEM-x6jikMYWIbE")
+@bot.command()
+async def search(ctx: Context, term: str):
+    results = google(term, my_api_key, my_cse_id, num=10)
+    await ctx.send(results[1]['title'] + ' ' + results[1]['formattedUrl'])
+
+@bot.command()
+async def wiki(ctx: Context, term: str):
+    results = google(term + ' site:en.wikipedia.org', my_api_key, my_cse_id, num=10)
+    await ctx.send(results[1]['title'] + ' ' + results[1]['formattedUrl'])
+
+with open("token") as f:
+    t = f.readline()
+
+bot.run(t)
